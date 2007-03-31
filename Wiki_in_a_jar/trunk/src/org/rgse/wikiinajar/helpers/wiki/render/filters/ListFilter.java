f500ba7b0ebc35628e062c4d0c620ec87ab1a9a9
rico_g <rico_g@45ce5d40-f82b-0410-9e75-ebb4a77ade87>
2007-03-31 22:27:43 +0000

    Added support for todo lists.

--- Wiki_in_a_jar/trunk/src/org/rgse/wikiinajar/helpers/wiki/render/filters/ListFilter.java
+++ Wiki_in_a_jar/trunk/src/org/rgse/wikiinajar/helpers/wiki/render/filters/ListFilter.java
@@ -30,138 +30,159 @@ import java.util.regex.Pattern;
  * 
  */
 public class ListFilter implements Filter {
-   
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.rgse.render.filter.Filter#filter(java.lang.String)
-     */
-    public String filter(String text) {
 
-        String result = text;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.rgse.render.filter.Filter#filter(java.lang.String)
+	 */
+	public String filter(String text) {
 
-        String regex = "(^([-#*]+|[-#*]*\\.)([^\r\n]+)[\r\n]*)+";
-        // String regex = "^==.*";
+		String result = text;
 
-        Pattern p = Pattern.compile(regex, Pattern.MULTILINE);
+		String regex = "(^([-#*]+|[-#*]*\\.)([^\r\n]+)[\r\n]*)+";
+		// String regex = "^==.*";
 
-        Matcher m = p.matcher(text);
+		Pattern p = Pattern.compile(regex, Pattern.MULTILINE);
 
-        while (m.find()) {
-            String plainList = m.group();
-            String formattedList = generateList(plainList);
-            int listStart = result.indexOf(plainList);
-            String first = result.substring(0, listStart);
-            String end = result.substring(listStart + plainList.length());
+		Matcher m = p.matcher(text);
 
-            result = first + formattedList + end;
-        }
-        return result;
+		while (m.find()) {
+			String plainList = m.group();
+			String formattedList = generateList(plainList);
+			int listStart = result.indexOf(plainList);
+			String first = result.substring(0, listStart);
+			String end = result.substring(listStart + plainList.length());
 
-    }
+			result = first + formattedList + end;
+		}
+		return result;
 
-    /**
-     * @param string
-     * @return
-     */
-    private static String generateList(String text) {
-        String[] lines = text.split("\n");
-        StringBuffer buffer = new StringBuffer();
-        Stack openTypes = new Stack();
-        int level = 0;
-        for (int i = 0; i < lines.length; i++) {
-            String line = lines[i].trim();
+	}
 
-            int lineLevel = extractLevel(line);
-            String curOpenType = extractOpenType(line);
-            if (lineLevel == 0) {
-                // not a list line
-                if (level > 0) {
-                    multiAppend(buffer, "</" + openTypes.peek() + "l>\n",
-                            openTypes, null, level);
-                    level = 0;
-                }
-            } else if (lineLevel > level) {
-                multiAppend(buffer, "<" + curOpenType + "l>\n", openTypes,
-                        curOpenType, lineLevel - level);
-                level = lineLevel;
-                // openTypes.push(curOpenType);
-            } else if (lineLevel < level) {
-                multiAppend(buffer, "</" + openTypes.peek() + "l>\n",
-                        openTypes, null, level - lineLevel);
-                level = lineLevel;
-            }
-            if (lineLevel > 0) {
-                if (!curOpenType.equals(openTypes.peek())) {
-                    buffer.append("\t</" + openTypes.pop() + "l><"
-                            + curOpenType + "l>\n");
-                    openTypes.push(curOpenType);
-                }
-                buffer.append("\t<li>" + line.substring(level).trim()
-                        + "</li>\n");
-            }
-        }
-        // close all open lists:
-        if (openTypes.size() > 0)
-            multiAppend(buffer, "</" + openTypes.peek() + "l>\n", openTypes,
-                    null, level);
+	/**
+	 * @param string
+	 * @return
+	 */
+	private static String generateList(String text) {
+		String[] lines = text.split("\n");
+		StringBuffer buffer = new StringBuffer();
+		Stack openTypes = new Stack();
+		int level = 0;
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i].trim();
 
-        return buffer.toString();
-    }
+			int lineLevel = extractLevel(line);
+			String curOpenType = extractOpenType(line);
+			if (lineLevel == 0) {
+				// not a list line
+				if (level > 0) {
+					multiAppend(buffer, "</" + openTypes.peek() + "l>\n",
+							openTypes, null, level);
+					level = 0;
+				}
+			} else if (lineLevel > level) {
+				multiAppend(buffer, "<" + curOpenType + "l>\n", openTypes,
+						curOpenType, lineLevel - level);
+				level = lineLevel;
+				// openTypes.push(curOpenType);
+			} else if (lineLevel < level) {
+				multiAppend(buffer, "</" + openTypes.peek() + "l>\n",
+						openTypes, null, level - lineLevel);
+				level = lineLevel;
+			}
+			if (lineLevel > 0) {
+				if (!curOpenType.equals(openTypes.peek())) {
+					buffer.append("\t</" + openTypes.pop() + "l><"
+							+ curOpenType + "l>\n");
+					openTypes.push(curOpenType);
+				}
+				String[] listData = extractListData(line.substring(level)
+						.trim());
+				buffer.append("\t<li " + listData[0] + " >" + listData[1]
+						+ "</li>\n");
+			}
+		}
+		// close all open lists:
+		if (openTypes.size() > 0)
+			multiAppend(buffer, "</" + openTypes.peek() + "l>\n", openTypes,
+					null, level);
 
-    /**
-     * @param line
-     * @return
-     */
-    private static String extractOpenType(String line) {
-        if (line.length() == 0)
-            return "";
-        switch (line.charAt(0)) {
-        case '*':
-            return "u";
-        case '-':
-            return "u";
-        case '#':
-            return "o";
-        default:
-            return "u";
-        }
-    }
+		return buffer.toString();
+	}
 
-    /**
-     * @param buffer
-     * @param string
-     * @param i
-     */
-    private static void multiAppend(StringBuffer buffer, String string,
-            Stack openTypes, String curOpenType, int count) {
-        for (int i = 0; i < count; i++) {
-            buffer.append(string);
-            if (curOpenType != null)
-                openTypes.push(curOpenType);
-            else
-                openTypes.pop();
-        }
-    }
+	/**
+	 * Checks if the list item starts with a to do marker
+	 * 
+	 * @param string
+	 * @return the list item id (for styling) as first element, the list item
+	 *         line as second.
+	 */
+	private static String[] extractListData(String string) {
+		if (string.startsWith("[x]")) {
+			return new String[]{"id=\"todo-done\"", string.substring(3).trim()};
+		} else if (string.startsWith("[!]")) {
+			return new String[]{"id=\"todo-imp\"", string.substring(3).trim()};
+		} else if (string.startsWith("[ ]")) {
+			return new String[]{"id=\"todo-normal\"", string.substring(3).trim()};
+		}
+		return new String[]{"", string};
+		
+	}
 
-    /**
-     * @param line
-     * @return
-     */
-    private static int extractLevel(String line) {
-        for (int i = 0; i < line.length(); i++) {
-            if (!isListChar(line.charAt(i)) || i == line.length() - 1)
-                return i;
-        }
-        return 0;
-    }
+	/**
+	 * @param line
+	 * @return
+	 */
+	private static String extractOpenType(String line) {
+		if (line.length() == 0)
+			return "";
+		switch (line.charAt(0)) {
+		case '*':
+			return "u";
+		case '-':
+			return "u";
+		case '#':
+			return "o";
+		default:
+			return "u";
+		}
+	}
 
-    /**
-     * @param c
-     * @return
-     */
-    private static boolean isListChar(char c) {
-        return c == '*' || c == '#' || c == '-';
-    }
+	/**
+	 * @param buffer
+	 * @param string
+	 * @param i
+	 */
+	private static void multiAppend(StringBuffer buffer, String string,
+			Stack openTypes, String curOpenType, int count) {
+		for (int i = 0; i < count; i++) {
+			buffer.append(string);
+			if (curOpenType != null)
+				openTypes.push(curOpenType);
+			else
+				openTypes.pop();
+		}
+	}
+
+	/**
+	 * @param line
+	 * @return
+	 */
+	private static int extractLevel(String line) {
+		for (int i = 0; i < line.length(); i++) {
+			if (!isListChar(line.charAt(i)) || i == line.length() - 1)
+				return i;
+		}
+		return 0;
+	}
+
+	/**
+	 * @param c
+	 * @return
+	 */
+	private static boolean isListChar(char c) {
+		return c == '*' || c == '#' || c == '-';
+	}
 
 }
