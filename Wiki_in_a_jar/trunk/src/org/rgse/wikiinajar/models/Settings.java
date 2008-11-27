08214c5f446586e9aabba224f3c1be2cd9b58061
rico_g <rico_g@45ce5d40-f82b-0410-9e75-ebb4a77ade87>
2008-11-27 23:56:48 +0000

    Tab support.

--- Wiki_in_a_jar/trunk/src/org/rgse/wikiinajar/models/Settings.java
+++ Wiki_in_a_jar/trunk/src/org/rgse/wikiinajar/models/Settings.java
@@ -27,6 +27,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import net.sf.wikiinajar.xrays.IServerSettings;
@@ -89,5 +91,13 @@ public class Settings implements IServerSettings {
 		}
 		return instance;
 	}
+
+	/**
+	 * Returns a list of tags to show as tab in the main menu bar.
+	 * @return tags as strings or empty list.
+	 */
+    public List getTagsForMainMenu() {
+        return Arrays.asList(new String[] {"wiki", "help", "l", "all", "foooo"});
+    }
 
 }
